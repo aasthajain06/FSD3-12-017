dd689ccaf5692889a1201b95bdac9570b3ecc4af
Aastha Jain <itsaasthajain08@gmail.com>
2026-09-11 15:42:34 +0530

    lab4

---
+++ lab4/users.js
@@ -0,0 +1,9 @@
+// we use in memory database
+let users = [
+  { id: 1, name: "John Doe", mob: "1234567890", email: "john.doe@example.com" },
+  { id: 2, name: "Jane Smith", mob: "9876543210", email: "jane.smith@example.com" }
+];
+
+let nextId = 3; 
+
+export const getAllUsers = () => users;
